804156038954542dc0c779d3f3272fedb5af2d70
DadoLinux <ziocleto@gmail.com>
2020-03-26 13:18:17 +0000

    playing with css flex grid layout component

--- projects/trends.bar/src/components/dashboardProject/subcomponents/LayoutEditor.js
+++ projects/trends.bar/src/components/dashboardProject/subcomponents/LayoutEditor.js
@@ -1,5 +1,19 @@
-import React, {Fragment} from "react";
+import React from "react";
+
+import GridLayout from 'react-grid-layout';
+import {DivLayout} from "./LayoutEditor.styled";
 
 export const LayoutEditor = () => {
-  return <Fragment/>
+  const layout = [
+    {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+    {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+    {i: 'c', x: 4, y: 0, w: 1, h: 2}
+  ];
+  return (
+    <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+      <DivLayout key="a">a</DivLayout>
+      <DivLayout key="b">b</DivLayout>
+      <DivLayout key="c">c</DivLayout>
+    </GridLayout>
+  )
 };
---
+++ projects/trends.bar/src/components/dashboardProject/subcomponents/LayoutEditor.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const DivLayout = styled.div` {
+  margin: 5px;
+  border: 1px solid var(--primary);
+  
+}`;
+
