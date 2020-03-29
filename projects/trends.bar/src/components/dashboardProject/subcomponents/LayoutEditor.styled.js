1b97b31b7d5f980c0869a5c949a2fafb8077f21e
DadoLinux <ziocleto@gmail.com>
2020-03-29 16:40:47 +0100

    Simple layout example

--- projects/trends.bar/src/components/dashboardProject/subcomponents/LayoutEditor.styled.js
+++ projects/trends.bar/src/components/dashboardProject/subcomponents/LayoutEditor.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const DivLayout = styled.div` {
   margin: 5px;
-  border: 1px solid var(--primary);  
+  border: 1px solid var(--light);
+  background-color: var(--secondary-alt-color);  
 }`;
 
