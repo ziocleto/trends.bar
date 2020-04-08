451412543c671b8a27f7f66f777b9a0a89c4ba57
DadoLinux <ziocleto@gmail.com>
2020-04-08 16:16:38 +0100

    incremental, I need to move the amgraphs to their own component!

--- projects/trends.bar/src/components/dashboardProject/subcomponents/ScriptEditor.js
+++ projects/trends.bar/src/components/dashboardProject/subcomponents/ScriptEditor.js
@@ -1,11 +1,19 @@
 import React from "reactn";
-import {Fragment, useEffect, useState} from "react";
+import {Fragment, useEffect, useRef, useState} from "react";
 import {Button, Col, Container, Dropdown, Form, InputGroup, Row, Tab, Table, Tabs} from "react-bootstrap";
 import {DangerColorSpan, DangerColorTd, FormGroupBorder, GroupTransform} from "./GatherEditor-styled";
 import {api, useApi} from "../../../futuremodules/api/apiEntryPoint";
 import {getCSVGraphKeys} from "../../../futuremodules/fetch/fetchApiCalls";
 import {LightColorTextSpanBold} from "../../../futuremodules/reactComponentStyles/reactCommon.styled";
 import {useTrendIdGetter} from "../../../modules/trends/globals";
+
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+import {getArrayFromJsonPath} from "../../../modules/trends/jsonPath";
+import {transformData} from "../../../modules/trends/dataTransformer";
+import uniqueId from "lodash/uniqueId";
+am4core.useTheme(am4themes_dark);
 
 const getLabelTransformOfGroup = (scriptJson, groupName) => {
   if ( !scriptJson ) return "None";
@@ -24,10 +32,94 @@ export const ScriptEditor = () => {
   const [scriptJson, setScriptJson] = useState(null);
   const fetchApi = useApi('fetch');
   const fetchResult = fetchApi[0];
+  const graphId = "hdsajdhakas";// useState(uniqueId('graph-'));
+
+  let chartRef = useRef();
 
   useEffect(() => {
     if (fetchResult) {
       setScriptJson(fetchResult.script);
+
+
+      // const graphData=[];
+      // for (let i=0;i<fetchResult.graphXYSeries.length;i++) {
+      //   try {
+      //     const serieData = getArrayFromJsonPath(data, config.graphXYSeries[i].query).map(e => {
+      //       const serieRow = {
+      //         label: transformData(e[config.graphXYSeries[i].fieldX], config.graphXYSeries[i].transformX),
+      //         y: transformData(e[config.graphXYSeries[i].fieldY], config.graphXYSeries[i].transformY)
+      //       }
+      //       return serieRow;
+      //     });
+      //     graphData.push({
+      //       name: config.graphXYSeries[i].title,
+      //       data: serieData
+      //     });
+      //   } catch (ex) {
+      //     console.log("Error building graphxy", ex)
+      //   }
+      // }
+
+      const graphData=[];
+      let visits = 10;
+      for (let i = 1; i < 366; i++) {
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+        graphData.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+      }
+
+      let chart = am4core.create(graphId, am4charts.XYChart);
+
+      chart.data = graphData;
+      chart.paddingRight = 20;
+
+      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = chart.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = "date";
+      series.dataFields.valueY = "value";
+
+      series.tooltipText = "{valueY.value}";
+      chart.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      chart.scrollbarX = scrollbarX;
+
+      // var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      // dateAxis.renderer.minGridDistance = 50;
+      // dateAxis.renderer.grid.template.location = 0.5;
+      // dateAxis.startLocation = 0.5;
+      // dateAxis.endLocation = 0.5;
+      //
+      // graphData.forEach((e,i) => {
+      //   console.log(`Serie: ${e.name}`);
+      //   console.log(`${JSON.stringify(e.data)}`);
+      //   let series = chart.series.push(new am4charts.LineSeries());
+      //   series.name=e.name;
+      //   series.dataFields.valueY = "y";
+      //   series.dataFields.dateX = "label";
+      //   series.tooltipText = "{dateX.formatDate(dd/MM)} - {name} {valueY.value}";
+      //   series.strokeWidth = 3;
+      //   series.tensionX = 0.8;
+      //   series.bullets.push(new am4charts.CircleBullet());
+      //   series.data = e.data;
+      // });
+
+      chart.cursor = new am4charts.XYCursor();
+
+      chartRef.current = chart;
+
+      return () => {
+        // console.log('will unmount');
+        // if (grafo) {
+        //   grafo.dispose();
+        // }
+      }
     }
   }, [fetchResult]);
 
@@ -163,6 +255,9 @@ export const ScriptEditor = () => {
                     </tbody>
                   </Table>
                 </FormGroupBorder>
+                <FormGroupBorder>
+                  {/*<div ref={chartRef} id={graphId} style={{width: "100%", height: "100%"}}/>*/}
+                </FormGroupBorder>
               </Tab>
             )
           })}
@@ -185,6 +280,7 @@ export const ScriptEditor = () => {
           </Form>
         </Col>
       </Row>
+      <div ref={chartRef} id={graphId} style={{width: "100%", height: "100%"}}/>
     </Container>
   );
 };
